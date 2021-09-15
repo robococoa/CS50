5adfa2cbe3abc0f40155ef48e2d89ad63f946274
robococoa <robococoa@users.noreply.github.com>
2021-09-15 05:25:54 +0000

    automated commit by submit50

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
-name = input("What is your name?")
+print("What is your name?")
 
-#print("{}".format(name = input('What is your name?')))
+# Place user input on a new line
+name = input()
 
+# Print response including input
 print(f"hello, {name}")
