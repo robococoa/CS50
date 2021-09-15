146c5e628eafd6a5202485b6b4617d7313f61282
robococoa <robococoa@users.noreply.github.com>
2021-09-15 06:29:29 +0000

    automated commit by check50 [check50=True]

---
+++ mario.py
@@ -0,0 +1,26 @@
+import cs50
+
+
+def main():
+    # Initialise height variable for user input
+    height = 0
+    row = 0
+    # Set height to user input for the desired pyramid height
+    while height < 1 or height > 8:
+        height = cs50.get_int("Height: ")
+
+    pyramid(height, row)
+
+# Recursively print pyramid to console
+
+
+def pyramid(height, row):
+    if height > 1:
+        pyramid(height - 1, row + 1)
+    print(" " * row, end="")
+    print("#" * height)
+
+    if __name__ == "__main__"
+
+
+main()
