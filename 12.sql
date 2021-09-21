7e00f2df2517e338e0ed4301bf3dd45cfb86d51f
robococoa <robococoa@users.noreply.github.com>
2021-09-21 02:32:57 +0000

    automated commit by submit50

--- 12.sql
+++ 12.sql
@@ -1,4 +1,7 @@
 SELECT title FROM movies
     JOIN stars ON movies.id = stars.movie_id
     JOIN people ON stars.person_id = people.id
-    WHERE movie_id IN (SELECT movie_id from stars WHERE name = "Johnny Depp" AND name = "Helena Bonham Carter");
+    WHERE movie_id IN
+    (SELECT movie_id from stars WHERE name = "Johnny Depp"
+    INTERSECT
+    SELECT movie_id from stars WHERE name = "Helena Bonham Carter");
