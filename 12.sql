bc8dcbbc5e83b0c099fa1ee7e5c0645f1acee539
robococoa <robococoa@users.noreply.github.com>
2021-09-21 00:34:39 +0000

    automated commit by check50 [check50=True]

---
+++ 12.sql
@@ -0,0 +1,4 @@
+SELECT title FROM movies
+    JOIN stars ON movies.id = stars.movie_id
+    JOIN people ON stars.person_id = people.id
+    WHERE movie_id IN (SELECT movie_id from stars WHERE name = "Johnny Depp" AND name = "Helena Bonham Carter");
